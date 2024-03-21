843f489734a3a08c5b1ba22b2db7ed580e83119d
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-21 10:05:43 -0700

    update tracker.astro

--- public/scripts/eruda.js
+++ public/scripts/eruda.js
@@ -1,9 +1,6 @@
-alert(!!document.querySelector('[eruda], .eruda'))
-if (!!document.querySelector('[eruda], .eruda')) {
-  var script = document.createElement('script'); 
-  script.src="//cdn.jsdelivr.net/npm/eruda"; 
-  document.body.appendChild(script); 
-  script.onload = function () {
-    eruda.init()
-  }
+var script = document.createElement('script'); 
+script.src="//cdn.jsdelivr.net/npm/eruda"; 
+document.body.appendChild(script); 
+script.onload = function () {
+  eruda.init()
 }
