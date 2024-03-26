5644587929a6550230916dff3f147bd636ca7aca
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-26 09:44:02 -0700

    update tracker

--- public/scripts/tracker.js
+++ public/scripts/tracker.js
@@ -50,7 +50,17 @@ function setMapPosition(entity, position) {
     <filter id='shadow' color-interpolation-filters="sRGB">
         <feDropShadow dx="0" dy="0" stdDeviation="40" flood-opacity="1" flood-color="#bcd2f7"/>
     </filter>
-</svg>`
+</svg>
+<style>
+    .map-icon.user .outer-group .outer {
+        animation: userIconOuter 5s ease-in-out infinite;
+    }
+    @keyframes userIconOuter {
+        to {
+            r: 364px;
+        }
+    }
+</style>`
     }
     var center = map.getCenter();
     console.log(position)
