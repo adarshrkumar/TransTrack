ba395b42213457caeb6a1924d8c7bc287f5b6a67
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-03-26 10:31:10 -0700

    update directions

--- public/scripts/directions.js
+++ public/scripts/directions.js
@@ -1,87 +1,42 @@
-// var d = new Date();
+var map
+var directionsElement = document.querySelector('#directions')
 
-// var mon = d.getMonth()
-// var day = d.getDate()
-// var yea = d.getFullYear()
-// var date = `${mon}/${day}/${yea}`
+var places = [
+    // '220 Demi Lane, Redwood City CA', 
+    'Milbrae BART', 
+    'Fishermans Wharf', 
+]
 
-// var time = 'AM'
-// var hur = d.getHours()
-// if (hur > 12) {
-//     hur -= 12
-//     time = 'PM'
-// }
-// var min = d.getMinutes()
-// var sec = d.getSeconds()
-// var time = `${hur}:${min}:${sec}${time}`
 
-// var places = [
-//     // '220 Demi Lane, Redwood City CA', 
-//     'Milbrae BART', 
-//     'Fishermans Wharf', 
-// ]
-// places.forEach(function(p, i) {
-//     places[i] = encodeURIComponent(p)
-// })
-// var newPlaces = places.slice(1, -1)
+function getMap() {
+    map = new Microsoft.Maps.Map('#myMap', {});
 
-// var options = [
-//     'travelMode', 
-//     'timeType', 
-//     'dateTime', 
-//     'maxSolutions',
-//     'distanceUnit',
-// ]
+    //Load the directions module.
+    Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
+        //Create an instance of the directions manager.
+        var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
+        
+        //Calculate a date time that is 1 hour from now.
+        var departureTime  = new Date();
+        departureTime.setMinutes(departureTime.getHours() + 1);
 
-// var optionData = {
-//     travelMode: 'Transit', 
-//     timeType: 'Departure', 
-//     dateTime: `${date}${''/* ${time}*/}`, 
-//     maxSolutions: 3,
-//     distanceUnit: 'Mile',
-// }
-// options.forEach(function(o, i) {
-//     optionData[o] = encodeURIComponent(optionData[o])
-// })
-// var wayPointStr = ''
-// wayPointStr += `wayPoint.1=${places[0]}&`
-// newPlaces.forEach(function(p, i) {
-//     wayPointStr += `viaWaypoint.${i+2}=${p}&`
-// })
-// wayPointStr += `waypoint.${places.length}=${places.slice(-1)}`
+        //Set Route Mode to transit.
+        directionsManager.setRequestOptions({
+            routeMode: Microsoft.Maps.Directions.RouteMode.transit,
+            time: departureTime,
+            timeType: Microsoft.Maps.Directions.TimeTypes.departure,
+        });
 
-// var url = `https://dev.virtualearth.net/REST/v1/Routes/${optionData.travelMode}?${wayPointStr}${/*&optimize={optimize}*/''}&timeType=${optionData.timeType}&dateTime=${optionData.dateTime}${/*&maxSolutions=${optionData.maxSolutions}*/''}&distanceUnit=${optionData.distanceUnit}&key=${BingMapsKey}`
+        //Add waypoints.
+        places.forEach(function(p, i) {
+            var waypoint = new Microsoft.Maps.Directions.Waypoint({ address: p });
+            directionsManager.addWaypoint(waypoint);
+        })
 
-// var routes = []
+        //Set the element in which the itinerary will be rendered.
+        directionsManager.setRenderOptions({ itineraryContainer: directionsElement });
 
-// var xhr = new XMLHttpRequest()
-// xhr.open('GET', url)
-// xhr.addEventListener('load', function() {
-//     var res = this.responseText
-//     if ((res.startsWith('{') || res.startsWith('[')) && typeof res === 'string') res = JSON.parse(res)
-//     if (!!res.errorDetails) {
-//         assertError(res.errorDetails, 'Transit Route')
-//         return
-//     }
-//     if (!!res.resourceSets) {
-//         res = res.resourceSets
-//         res.forEach(function(s, i) {
-//             if (!!s.resources) {
-//                 s = s.resources
-//                 s.forEach(function(r, i2) {
-//                     routes.push(r)
-//                     addRouteItems(routes);
-//                 })
-//             }
-//         })
-//     }
-//     else {
-//         alert(res)
-//         console.log(res)
-//     }
-// })
-// xhr.send()
-
-// function addRouteItems(rS) {
-
-// }
+        //Calculate directions.
+        directionsManager.calculateDirections();
+    });
+}
