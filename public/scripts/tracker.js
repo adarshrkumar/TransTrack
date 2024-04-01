18ee7624c1e4cb7611441edc8306bb1b83ef3636
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-04-01 11:44:00 -0700

    update tracker.js

--- public/scripts/tracker.js
+++ public/scripts/tracker.js
@@ -24,53 +24,53 @@ function GetMap() {
 
 window.addEventListener('DOMContentLoaded', GetMap)
 
-if ("geolocation" in navigator) {
-    /* geolocation is available */
-    window.addEventListener('DOMContentLoaded', function(e) {
-        watchID = navigator.geolocation.watchPosition((position) => {
-            var errorMargin = 0;
-            var obj = {
-                latitude: position.coords.latitude + errorMargin, 
-                longitude: position.coords.longitude + errorMargin, 
-                altitude: 0, 
-                altitudeReference: -1
-            }
-            setMapPosition('user', obj);
-        }, function(err) {
-            assertError(err, 'Current Location')
-        }, { enableHighAccuracy: true });
-    })
-}
+// if ("geolocation" in navigator) {
+//     /* geolocation is available */
+//     window.addEventListener('DOMContentLoaded', function(e) {
+//         watchID = navigator.geolocation.watchPosition((position) => {
+//             var errorMargin = 0;
+//             var obj = {
+//                 latitude: position.coords.latitude + errorMargin, 
+//                 longitude: position.coords.longitude + errorMargin, 
+//                 altitude: 0, 
+//                 altitudeReference: -1
+//             }
+//             setMapPosition('user', obj);
+//         }, function(err) {
+//             assertError(err, 'Current Location')
+//         }, { enableHighAccuracy: true });
+//     })
+// }
 
-function setMapPosition(entity, position) {
-    var icon = ''
-    switch(entity) {
-        case 'user': 
-            icon += `<svg class="map-icon user" version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 864 864" width="64" height="64">
-    <g class="outer-group" fill="#4a86e8" style="filter: url(#shadow)">
-        <circle class="outer" cx="432" cy="432" r="340" />
-    </g>
-    <g fill="#ffffff">
-        <circle cx="432" cy="432" r="332" />
-    </g>
-    <g fill="#4a86e8">
-        <circle cx="432" cy="432" r="300" />
-    </g>
-    <g fill="#4a86e8">
-        <circle cx="432" cy="432" r="296" />
-    </g>
-    <filter id='shadow' color-interpolation-filters="sRGB">
-        <feDropShadow dx="0" dy="0" stdDeviation="40" flood-opacity="1" flood-color="#bcd2f7" />
-    </filter>
-</svg>`
-    }
-    console.log(position)
-    var pSize = 17
-    var pin = new Microsoft.Maps.Pushpin(position, {
-        icon: icon,
-        anchor: new Microsoft.Maps.Point(pSize, pSize)
-    });
+// function setMapPosition(entity, position) {
+//     var icon = ''
+//     switch(entity) {
+//         case 'user': 
+//             icon += `<svg class="map-icon user" version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 864 864" width="64" height="64">
+//     <g class="outer-group" fill="#4a86e8" style="filter: url(#shadow)">
+//         <circle class="outer" cx="432" cy="432" r="340" />
+//     </g>
+//     <g fill="#ffffff">
+//         <circle cx="432" cy="432" r="332" />
+//     </g>
+//     <g fill="#4a86e8">
+//         <circle cx="432" cy="432" r="300" />
+//     </g>
+//     <g fill="#4a86e8">
+//         <circle cx="432" cy="432" r="296" />
+//     </g>
+//     <filter id='shadow' color-interpolation-filters="sRGB">
+//         <feDropShadow dx="0" dy="0" stdDeviation="40" flood-opacity="1" flood-color="#bcd2f7" />
+//     </filter>
+// </svg>`
+//     }
+//     console.log(position)
+//     var pSize = 17
+//     var pin = new Microsoft.Maps.Pushpin(position, {
+//         icon: icon,
+//         anchor: new Microsoft.Maps.Point(pSize, pSize)
+//     });
 
-    //Add the pushpin to the map
-    map.entities.push(pin);
-}
+//     //Add the pushpin to the map
+//     map.entities.push(pin);
+// }
