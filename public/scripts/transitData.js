c65ab4d863f1155e169863a8d7c0c43710c3a2d3
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-04-16 09:17:06 -0700

    update transitData.js

--- public/scripts/transitData.js
+++ public/scripts/transitData.js
@@ -35,11 +35,12 @@ function addVehicles(data, i) {
         var vehicleActivity = vehicle.MonitoredVehicleJourney
         var vehicleLocation = vehicleActivity.VehicleLocation
 
-        var color = ''
+        var color = 'black'
+        var route = vehicleActivity.LineRef
 
         
         pin = new Microsoft.Maps.Pushpin(vehicleLocation, {
-            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 30"><rect x="0" y="0" width="40" height="30" fill="${color ? color : 'black'}" /><text x="50%" y="50%" dy="2" textLength="35" lengthAdjust="spacing" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${vehicleActivity.LineRef}</text></svg>`,
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 30"><rect x="0" y="0" width="40" height="30" fill="${color}" /><text x="50%" y="50%" dy="2" textLength="35" lengthAdjust="spacing" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${route}</text></svg>`,
             // title: 'Microsoft',
             // subTitle: 'City Center',
             // text: vehicleActivity.LineRef
