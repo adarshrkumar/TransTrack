68316534f0be143d2d8c86a255d1613132670395
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-04-16 11:38:38 -0700

    update tracker.js

--- public/scripts/tracker.js
+++ public/scripts/tracker.js
@@ -57,7 +57,7 @@ function onMapLoad() {
                     var pin = new Microsoft.Maps.Pushpin(vehicleLocation, {
                         // text: route,
                         color: color, 
-                        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="20"><rect x="0" y="0" width="100%" height="100%" fill="${color}" /><text x="50%" y="50%" dy="2" style="textLength="${width-35}" lengthAdjust="spacing" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${route}</text></svg>`,
+                        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="20"><rect x="0" y="0" width="100%" height="100%" fill="${color}" /><text x="50%" y="50%" dy="2" style="textLength="${width-5}" lengthAdjust="spacing" font-family="sans-serif" dominant-baseline="middle" text-anchor="middle">${route}</text></svg>`,
                         // title: 'Microsoft',
                         // subTitle: 'City Center',
                     });
