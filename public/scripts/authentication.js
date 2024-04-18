d9da609af5e43eabe73eec61e3e65f29a0d79260
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-04-17 19:39:49 -0700

    update authentication.js

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -12,11 +12,12 @@ var pathname = location.pathname
 if (!isAuthenticated && !pathname.startsWith('/login')) location.href = '/login'
 else if (isAuthenticated && pathname === '/') location.href = '/tracker'
 
-function setCredentials(username, password) {
+function setCredentials(username, password, dest) {
     // alert(JSON.stringify({username: username, password: password}))
     authentication = JSON.stringify({
         username: username,
         password: btoa(password),
     })
     setCookie('auth', authentication, 30)
+    location.href = dest
 }
