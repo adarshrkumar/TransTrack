048eccbcfd89e36f9c5dcc43821ca09ae908d8ae
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-04-18 08:23:09 -0700

    update authentication.js

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -5,12 +5,17 @@ authentication = authentication ? JSON.parse(authentication) : {}
 var username = authentication.username ? authentication.username : ''
 var password = authentication.password ? atob(authentication.password) : ''
 
-var isAuthenticated = false
-if (username && password) isAuthenticated = true
+var isAuthenticated = checkAuthentication(username, password)
 
 var pathname = location.pathname
 if (!isAuthenticated && !pathname.startsWith('/login')) location.href = '/login'
 else if (isAuthenticated && pathname === '/') location.href = '/tracker'
+
+function checkAuthentication(u, p) {
+    var a = false
+    if (u && p) a = true
+    return a
+}
 
 function setCredentials(username, password, dest) {
     authentication = JSON.stringify({
