4fef4ac480acbdcbf99ee2a21b088d542a259b89
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-05-07 11:41:16 -0700

    udpate index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,9 +3,14 @@ var urlParams = new URLSearchParams(queryString)
 
 var ratio = urlParams.get('ratio')
 if (ratio) {
-  if (ratio.includes('/')) ratio = ratio.split('/').join(' / ')
-  if (ratio.includes('  ')) ratio = ratio.split('  ').join(' ')
-  document.body.style.aspectRatio = ratio
+  if (ratio.includes(' / ')) ratio = ratio.split(' / ').join('/')
+  if (ratio.includes(' ')) ratio = ratio.split(' ').join('')
+
+  var parts = ratio.split('/')
+
+  var value = document.body.height*parts[0]/parts[1]
+
+  document.body.style.width = value
 }
 
 window.addEventListener('DOMContentLoaded', function(e) {
