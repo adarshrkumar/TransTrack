909de35db54119fbfd53f96743d8f66f294e6f10
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-06-19 15:03:31 -0700

    update 511Request.js

--- public/scripts/511Request.js
+++ public/scripts/511Request.js
@@ -1,11 +1,32 @@
 var apiKeys = [
-    '7a7f827d-6d7d-451e-a9a6-bcc517c951b9', 
-    '6e4e4158-72bd-424d-85a3-2678e0a9854e', 
-    '1eb78f53-b000-42ea-8ff6-9bedb66e01cc', 
-    '1bd488ee-e387-49a9-af4f-6a15d1636bff', 
-    'fac23cdd-333b-41fc-b6d2-fc3628fbfb1d', 
-    'b9269045-ce6b-48e3-9ef2-bdb1332499ad', 
-    '26c601e1-1221-4690-9b03-1d597f5ccc96', 
+    {
+        usage: 0, 
+        key: '7a7f827d-6d7d-451e-a9a6-bcc517c951b9', 
+    }, 
+    {
+        usage: 0, 
+        key: '6e4e4158-72bd-424d-85a3-2678e0a9854e', 
+    }, 
+    {
+        usage: 0, 
+        key: '1eb78f53-b000-42ea-8ff6-9bedb66e01cc', 
+    }, 
+    {
+        usage: 0, 
+        key: '1bd488ee-e387-49a9-af4f-6a15d1636bff', 
+    }, 
+    {
+        usage: 0, 
+        key: 'fac23cdd-333b-41fc-b6d2-fc3628fbfb1d', 
+    }, 
+    {
+        usage: 0, 
+        key: 'b9269045-ce6b-48e3-9ef2-bdb1332499ad', 
+    }, 
+    {
+        usage: 0, 
+        key: '26c601e1-1221-4690-9b03-1d597f5ccc96', 
+    }, 
 ]
 
 var errorModules = []
@@ -23,6 +44,10 @@ function makeRequest(moduleName, params=[], callback, i=0) {
         }
     }
 
+    if (apiKeys[i].usage < 100) {
+        makeRequest(moduleName, params, callback, i+1)
+        return
+    }
     var url = `https://api.511.org/transit/${moduleName}?api_key=${apiKeys[i]}${params}`
     // console.log(url)
 
@@ -35,6 +60,7 @@ function makeRequest(moduleName, params=[], callback, i=0) {
                     (data.startsWith('[') && data.endsWith(']'))
                 ) data = JSON.parse(data)
             }
+            apiKeys[i].usage++
             callback(data)
         })
         .catch(err => {
@@ -44,6 +70,7 @@ function makeRequest(moduleName, params=[], callback, i=0) {
             else {
                 console.error(err);
             }
+            apiKeys[i].usage++
         });
 
 }
