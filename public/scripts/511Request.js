d69bff09eb4d518df3d4720a9f1418353c6ce465
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-07-08 19:43:08 -0700

    updatae 511Request.js

--- public/scripts/511Request.js
+++ public/scripts/511Request.js
@@ -1,35 +1,14 @@
-var apiKeys = [
-    {
-        usage: 0, 
-        key: '7a7f827d-6d7d-451e-a9a6-bcc517c951b9', 
-    }, 
-    {
-        usage: 0, 
-        key: '6e4e4158-72bd-424d-85a3-2678e0a9854e', 
-    }, 
-    {
-        usage: 0, 
-        key: '1eb78f53-b000-42ea-8ff6-9bedb66e01cc', 
-    }, 
-    {
-        usage: 0, 
-        key: '1bd488ee-e387-49a9-af4f-6a15d1636bff', 
-    }, 
-    {
-        usage: 0, 
-        key: 'fac23cdd-333b-41fc-b6d2-fc3628fbfb1d', 
-    }, 
-    {
-        usage: 0, 
-        key: 'b9269045-ce6b-48e3-9ef2-bdb1332499ad', 
-    }, 
-    {
-        usage: 0, 
-        key: '26c601e1-1221-4690-9b03-1d597f5ccc96', 
-    }, 
+var api_keys = [
+    '7a7f827d-6d7d-451e-a9a6-bcc517c951b9', 
+    '6e4e4158-72bd-424d-85a3-2678e0a9854e', 
+    '1eb78f53-b000-42ea-8ff6-9bedb66e01cc', 
+    '1bd488ee-e387-49a9-af4f-6a15d1636bff', 
+    'fac23cdd-333b-41fc-b6d2-fc3628fbfb1d', 
+    'b9269045-ce6b-48e3-9ef2-bdb1332499ad', 
+    '26c601e1-1221-4690-9b03-1d597f5ccc96', 
 ]
-
-var errorModules = []
+var apiKeys = []
+api_keys.forEach(k => apiKeys.push({usage: 0, key: k}))
 
 function makeRequest(moduleName, params=[], callback, i=0) {
     if (Array.isArray(params)) {
