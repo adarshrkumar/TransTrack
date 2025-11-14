6f62aefa6b68f14ad5f2978f9d1ac5d59257b5a2
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-11-13 17:34:44 -0800

    u

--- public/scripts/eruda.js
+++ public/scripts/eruda.js
@@ -2,5 +2,5 @@ var script = document.createElement('script');
 script.src="//cdn.jsdelivr.net/npm/eruda"; 
 document.body.appendChild(script); 
 script.onload = function () {
-  eruda.init()
+    eruda.init()
 }
