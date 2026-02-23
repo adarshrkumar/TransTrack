60e4e744de38cffcb0576d5e63a05b8b288c370c
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-02-22 21:25:42 -0800

    u

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="astro/client" />
+
+interface Window {
+    makeRequest: (moduleName: string, params?: string[][], callback?: (data: unknown) => void) => Promise<void>
+    checkAuthentication: (u: string, p: string) => boolean
+    setCredentials: (username: string, password: string, dest: string) => void
+    setCookie: (cname: string, cvalue: string, exdays: number) => void
+    getCookie: (cname: string) => string
+    assertError: (err: unknown, name: string) => void
+    goToPage: (name: string) => void
+    showModal: (title: string, content: string) => void
+    addNotification: (module: string, name: string, icon: string, content: string, attatchmentInfo: unknown) => void
+    getNotifications: (module?: unknown, title?: unknown, description?: unknown, content?: unknown, attatchmentInfo?: unknown) => void
+    openSelection: (e: HTMLElement) => void
+    closeSelection: (e: HTMLElement) => void
+    showRoutePath: (aId: unknown, rId: unknown) => void
+    findUsingValueofKey: (arr: Record<string, unknown>[], key: string, val: unknown) => Record<string, unknown>[]
+}
